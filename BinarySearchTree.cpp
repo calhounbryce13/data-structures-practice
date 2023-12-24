c7aaa7bf3974cfd9edc60c185a4a24ca693b3db6
calhounbryce13 <calhounbryce13@gmail.com>
2023-12-24 10:53:06 -0800

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,1 +1,92 @@
+/*
+    Author: Bryce Calhoun
+    Description: 
+*/
+#include <iostream>
+
+using namespace std;
+
+class node{
+    /*
+
+    */
+
+    // access specifier
+    private:
+
+        // member variable
+        int value;
+
+    public:
+    node *left;
+    node *right;
+
+
+    // accessor
+    int get_value(){
+
+        return this->value;
+    }
+
+    // mutator
+    void set_value(int value){
+
+        this->value = value;
+    }
+
+};
+
+class binary_search_tree{
+    /*
+    
+    */
+
+    private:
+        int height;
+
+    public:
+        node *root;
+        binary_search_tree(){
+            this->height = 0;
+            this->root = NULL;
+        };
+        void insert(int);
+        node* node_prep(int);
+    
+
+};
+
+int main(){
+    binary_search_tree tree;
+    tree.insert(5);
+    
+}
+
+void binary_search_tree::insert(int value){
+    /*
+    
+    */    
+    node *ptr = this->node_prep(value);
+
+    if(this->root == NULL){
+        this->root = ptr;
+        cout << "here" << endl;
+    }
+
+}
+
+node* binary_search_tree::node_prep(int value){
+    /*
+    
+    */   
+    node *ptr = new node;
+    ptr->set_value(value);
+    ptr->left = NULL;
+    ptr->right = NULL;
+
+    return ptr;
+
+}
+
+
 

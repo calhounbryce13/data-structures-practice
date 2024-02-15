1e042785912b593032518bb0f7f2481ef5c6145b
calhounbryce13 <calhounbryce13@gmail.com>
2024-02-14 23:28:54 -0800

    updates

--- Static_Array/staticArray.py
+++ Static_Array/staticArray.py
@@ -1,1 +1,18 @@
+class Static_Array_Exception(Exception):
+
+
+    pass
+
+
+class Static_Array:
+    def __init__(self, capacity=10):
+        """
+        Initializing the size of the array and creating a list with the
+        specified size
+        """
+        self.capacity = capacity
+        self.data = [None] * capacity
+
+        if capacity < 0:
+            raise Static_Array_Exception("\nCapacity Must Be Positive !\n")
 
